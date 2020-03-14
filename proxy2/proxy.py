9dead64a548b0053a53c33f7140f7769429fa8a8
Prateek Sahu <prateeks@utexas.edu>
2020-03-14 02:54:20 -0500

    multi-threading

--- proxy2/proxy.py
+++ proxy2/proxy.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 from multiprocessing import Process
-import socket 
+import threading
+import socket
 import logging
 from jaeger_client import Config
 from opentracing.ext import tags
 from opentracing.propagation import Format
+from scapy.all import AsyncSniffer,wrpcap
 #import requests
 import sys
 import time
@@ -31,6 +33,16 @@ tracer = Config(
     service_name=os.environ['SERVICE'],
 ).initialize_tracer()
 
+# Multi-threading
+class myThread(threading.Thread):
+    def __init__(self, con, client_addr, tracer):
+        threading.Thread.__init__(self)
+        self.con = con
+        self.client_addr = client_addr
+        self.tracer = tracer
+    def run(self):
+        proxy_process(self.con, self.client_addr, self.tracer)
+
 def main():
 
     #in the pod everything is localhost
@@ -44,25 +56,44 @@ def main():
             s.close()
         print(f"Could not open socket {message}")
         exit(1)
+    #this line starts taking pcaps for us
+    p = Process(target=make_pcaps, args=())
+    p.start()
 
     while 1:
         #listen loop for port 8000
         #open socker on port 8000
-        con,clint_addr = s.accept()
-        p = Process(target=proxy_process, args=(con,clint_addr,tracer))
-        p.start()
+        con,client_addr = s.accept()
+        # Multi-processing
+        #p = Process(target=proxy_process, args=(con,clint_addr,tracer))
+        #p.start()
+
+        # Multi-threading
+        t = myThread(con, client_addr, tracer)
+        t.start()
+
         #listen to socket and receive any connection
     s.close()
+
+def make_pcaps():
+    #don't think I'll need more but make an hour worth of
+    #one minute pcap files
+    for i in range(60):
+        #if we need we can narrow the interface we use to sniff by changing
+        #the args to AsyncSniffer()
+        t = AsyncSniffer()
+        t.start()
+        time.sleep(60)
+        z = t.stop()
+        wrpcap(f"test_{i}.pcap", z)
 
 def proxy_process(connection, clint_address, tracer):
     #Should surround in try catch to get good error codes
     try:
         #recieve the data from the incoming socket
         request = connection.recv(MAX)
-        print('modifying request')
+        print('modifying request: '+str(request))
         data, headers = get_http_headers(request)
-        #print(data)
-        #print(headers)
         span_ctx = tracer.extract(Format.HTTP_HEADERS, headers)
         op_name = ''
         span_tags = {}
@@ -75,10 +106,10 @@ def proxy_process(connection, clint_address, tracer):
             span_tags = {tags.SPAN_KIND: tags.SPAN_KIND_RPC_SERVER}
             child_ctx = 1
         with tracer.start_active_span(op_name, child_of=span_ctx, tags=span_tags) as scope:
-        #with scope = 1:
             span = tracer.active_span
             span.set_tag(tags.HTTP_METHOD, 'GET')
-            modify_http(data, headers, span, tracer, child_ctx)
+            data = modify_http(data, headers, span, tracer, child_ctx)
+            print('modified request: '+str(data))
             #make a new socket to forward the data
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((dst_server, dst_port))
@@ -111,15 +142,13 @@ def modify_http(data, headers, span, tracer, child):
     tracer.inject(span_context=span.context, format=Format.HTTP_HEADERS, carrier=carrier)
     print(carrier)
     d = data.split(b"\r\n")
-    pos = 0
-    #print(headers)
-    headers['uber-trace-id'] = carrier.get('uber-trace-id')
-    #print(headers)
-    d1 = [(str(t)+': '+str(headers.get(t))).encode('utf-8') for t in headers]
-    #print(d)
-    #print(d1)
-    d = d1 + d
-    #print(d)
+    #k, v = 'uber-trace-id', carrier.get('uber-trace-id')
+    #if 'uber-trace-id' not in headers:
+    #    headers.insert(len(headers)-2, k)
+    #headers[k] = v
+    carrier.update(headers)
+    d1 = [(str(t)+': '+str(carrier.get(t))).encode('utf-8') for t in carrier]
+    d = [d[0]] + d1 + d[1:]
     #after you modify the line you need
     #put the data back together again
     data = b"\r\n".join(d)
@@ -130,18 +159,21 @@ def get_http_headers(data):
     d = data.split(b"\r\n")
     headers = {}
     pos = 0
+    non_headers = []
     for i in d:
         if(i == b''):
             break
+        pos=pos+1
         if(b': ' not in i):
-            print(i)
+            non_headers.append(pos)
             continue
         i_str = i.decode('utf-8').split(": ")
         headers[i_str[0]] = i_str[1]
-        pos=pos+1
     #after you modify the line you need
     #put the data back together again
-    data = b"\r\n".join(d[pos:])
+    d_new = [d[0]]+d[pos:]
+    print(d_new)
+    data = b"\r\n".join(d_new)
     return data, headers
 
 if __name__ == "__main__":
