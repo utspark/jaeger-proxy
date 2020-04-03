2bf392ba24dda37331e8c0a27097975870c28ab2
Prateek Sahu <prateeks@utexas.edu>
2020-04-02 20:39:34 -0500

    jaeger operator script update

--- deploy_operator.sh
+++ deploy_operator.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
-git clone git@github.com:jaegertracing/jaeger-operator.git
-cd jaeger-operator
+action=$1
 
-#deploy jaeger operator
-kubectl create ns observability
-kubectl create -n observability -f deploy/crds/jaegertracing.io_jaegers_crd.yaml
-kubectl create -n observability -f deploy/service_account.yaml
-kubectl create -n observability -f deploy/role.yaml
-kubectl create -n observability -f deploy/role_binding.yaml
-kubectl create -n observability -f deploy/operator.yaml
+if [ $action == "create" ]
+then
+  git clone git@github.com:jaegertracing/jaeger-operator.git
+  action='apply'
+fi
 
-kubectl apply -n observability -f deploy/examples/simplest.yaml
+if [ $action == "apply" ]
+then
+  
+  cd jaeger-operator
+
+  #deploy jaeger operator
+  kubectl create ns observability
+  kubectl create -n observability -f deploy/crds/jaegertracing.io_jaegers_crd.yaml
+  kubectl create -n observability -f deploy/service_account.yaml
+  kubectl create -n observability -f deploy/role.yaml
+  kubectl create -n observability -f deploy/role_binding.yaml
+  kubectl create -n observability -f deploy/operator.yaml
+
+  cd ../
+  kubectl apply -f all-in-one-with-options.yaml
+elif [ $action == "delete" ]
+then
+  cd jaeger-operator
+
+  kubectl delete -n observability -f deploy/crds/jaegertracing.io_jaegers_crd.yaml
+  kubectl delete -n observability -f deploy/service_account.yaml
+  kubectl delete -n observability -f deploy/role.yaml
+  kubectl delete -n observability -f deploy/role_binding.yaml
+  kubectl delete -n observability -f deploy/operator.yaml
+
+  cd ../
+  kubectl delete -f all-in-one-with-options.yaml
+  kubectl delete ns observability
+fi
